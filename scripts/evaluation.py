4ada71aabd4a6791eb8339c00d3dcec505a68c4f
Johnny Li <iamjli@mit.edu>
2018-11-12 00:13:55 -0500

    Finish evaluation module

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -1,56 +1,30 @@
+#!/usr/bin/env python3
 
-"""
+# Core python modules
+import os
+import multiprocessing
+import logging
 
-This file contains methods to evaluate different models performance on synthetic and known data. 
-
-
- * signal sampler (pure source sources)
- 	* n_sources
- 	* signal support size
- 	* [ sampler kernel ]
- * signal reconstruction
- 	* n_samples
- 	* noise
- 	* [ mixing proportions ]
-
- * model
- 	* model
- 	* model param
-
-
-Variable scheme
-
-Data generation
-	Signal
-	Mixing
-	Noisy data 
-	(data not needed because can be generated easily)
-
-Separately: n_samples. Remove this from grid. 
-
-
-
-0) For each param except for `n_samples` and `noise`, 
-
-1) Sample `n_sources` source signal(s) from the signal generator into a 2D numpy array, 
-   where each column is a signal component and each row is a gene. Set as param attribute
-
-2) Initialize loadings (coeffs corresponding to contribution of each component) as a 2D np array
-   where each column is the latent representation for a single sample, and rows correspond to sources.
-
-3) Reconstruct data by dot product and add `noise` 
-
-
-"""
+from itertools import product
 
 import numpy as np
-
 from sklearn.model_selection import ParameterGrid
 
+from scoring import recovery_relevance, precision_recall
 
 
 
-def subset_dict(dictionary, keys): return dict((key, dictionary[key]) for key in dictionary.keys() & set(keys))
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+handler.setFormatter(logging.Formatter('%(asctime)s - Evaluate: %(levelname)s - %(message)s', "%I:%M:%S"))
+logger.addHandler(handler)
+
+
+# Count the number of available CPUs for potential use in multiprocessing code
+try: n_cpus = int(os.environ["SLURM_JOB_CPUS_PER_NODE"])
+except KeyError: n_cpus = multiprocessing.cpu_count()
 
 
 class Evaluate: 
@@ -62,65 +36,78 @@ class Evaluate:
 		self.param_grid  = param_grid
 
 		# Ensure param_grid has the necessary attributes
-		assert 'n_sources' in param_grid
-		assert 'n_samples' in param_grid
-		assert 'support_size' in param_grid
-		assert 'noise' in param_grid
+		for attribute in [ "n_sources", "n_samples", "support_size", "noise", "rep" ]: 
+			assert attribute in param_grid
 
-		print("Parameter grid loaded:", subset_dict(self.param_grid, ['n_sources', 'support_size']))
-
-		# Set lookup dictionaries to sample from for different parameter sets
-		self.source_matrices, self.loading_matrices = self._matrix_lookups()
-
-		# Generate data matrices for each parameter set in the same order as 
-		self.params_iterator = ParameterGrid(self.param_grid)
-		self.data_matrices = [ self._data_matrix(**params) for params in self.params_iterator ]
+		# Create lookup tables
+		self._matrix_lookups()
+		self.data_params = ParameterGrid(self.param_grid)
 
 
-	########  EVALUATE  ########
+	########  DATA GENERATION  ######## 
 
-	def evaluate(self, ): 
-		pass
-
-
-	########  DATA GENERATION  ########
+	def _param_iterator(self, keys): 
+		# Returns an iterator through parameter sets with specified keys
+		return product(*[ self.param_grid[key] for key in keys ])
 
 	def _source_matrix(self, support_size, n_sources):
-
 		# Matrix of genes x sources (aka components)
 		# This should be shared across all parameter sets with the same `n_sources` and `support_size_vals`
 		sources = np.array([ self.sampler(self.graph, support_size) for _ in range(n_sources) ]).T  
 		return sources
 
 	def _loadings_matrix(self, n_sources, n_samples):
-
 		# Matrix of sources x sample latent values 
 		loadings = np.random.rand(n_sources, n_samples)
 		return loadings
 
-	def _matrix_lookups(self): 
-
-		# Dictionary of source signals indexed by (n_sources, support_size)
-		source_param_grid = ParameterGrid( subset_dict(self.param_grid, ['support_size', 'n_sources']) )
-		source_matrices = { (params['support_size'], params['n_sources']) : self._source_matrix(**params) for params in source_param_grid }
-
-		# Dictionary of loadings indexed by (n_sources, n_samples)
-		loading_param_grid = ParameterGrid( subset_dict(self.param_grid, ['n_sources', 'n_samples']) )
-		loading_matrices = { (params['n_sources'], params['n_samples']) : self._loadings_matrix(**params) for params in loading_param_grid }
-
-		return source_matrices, loading_matrices
-
-
-	def _data_matrix(self, support_size, n_sources, n_samples, noise): 
-
-		sources = self.source_matrices[(support_size, n_sources)]
-		loadings = self.loading_matrices[(n_sources, n_samples)]
-
-		# Add noise to the reconstructed and add to params dict
-		# Matrix of genes x samples
+	def _data_matrix(self, support_size, n_sources, n_samples, noise, rep): 
+		# Load corresponding source and loading matrices
+		sources = self.get_sources(support_size, n_sources, rep)
+		loadings = self.get_loadings(n_sources, n_samples, rep)
+		# Reconstruct data matrix by taking dot product and adding noise
 		data = np.random.normal( sources.dot(loadings), scale=noise)
 		return data
 
+	def _matrix_lookups(self): 
 
+		# Dictionary of source signals indexed by (n_sources, support_size)
+		source_params = self._param_iterator(['support_size', 'n_sources', 'rep'])
+		self.source_matrices = { (support_size, n_sources, rep) : self._source_matrix(support_size, n_sources) for support_size,n_sources,rep in source_params }
+
+		# Dictionary of loadings indexed by (n_sources, n_samples)
+		loading_params = self._param_iterator(['n_sources', 'n_samples', 'rep'])
+		self.loading_matrices = { (n_sources, n_samples, rep) : self._loadings_matrix(n_sources, n_samples) for n_sources,n_samples,rep in loading_params }
+
+		# Dictionary of data matrices
+		data_params = self._param_iterator(['support_size', 'n_sources', 'n_samples', 'noise', 'rep'])
+		self.data_matrices = { params : self._data_matrix(*params) for params in data_params }
+
+
+	########  EVALUATION  ######## 
+
+	def evaluate(self, model): 
+
+		with multiprocessing.Pool(n_cpus) as pool: 
+
+			data = [ self.get_data(**params) for params in self.data_params ]
+			results = pool.map(model, data)
+
+			comparisons = [ (result, self.get_sources(**params).T) for result,params in zip(*[results, self.data_params]) ]
+			scores = pool.starmap(recovery_relevance, comparisons)
+
+		return scores
+
+
+	########  DATA QUERY  ######## 
+
+	def get_sources(self, support_size, n_sources, rep, **kwargs): 
+		return self.source_matrices[(support_size, n_sources, rep)]
+
+	def get_loadings(self, n_sources, n_samples, rep, **kwargs): 
+		return self.loading_matrices[(n_sources, n_samples, rep)]
+
+	def get_data(self, support_size, n_sources, n_samples, noise, rep, **kwargs): 
+		return self.data_matrices[(support_size, n_sources, n_samples, noise, rep)]
 
 
---
+++ scripts/scoring.py
@@ -0,0 +1,81 @@
+ 
+
+import numpy as np
+from scipy import stats
+
+
+# These functions are used to score model results against known results
+
+
+def recovery_relevance(model, known): 
+
+	# If signal value is non-zero, assign to 1
+	supp1 = (model != 0).astype(float)
+	supp2 = (known != 0).astype(float) 
+
+	# For each pair of modules, find number of nodes in intersect and union
+	# Dot product of two supports gives number of nodes in intersection
+	intersect = supp1.dot(supp2.T)
+	union = supp1.shape[1] - (1-supp1).dot(1-supp2.T) 
+	jaccard_matrix = intersect / union
+
+	recovery  = jaccard_matrix.max(axis=0).mean()
+	relevance = jaccard_matrix.max(axis=1).mean()
+	if recovery * relevance == 0: F1rr = 0
+	else: F1rr = stats.hmean([recovery, relevance])
+
+	scores = {
+		"recovery": recovery, 
+		"relevance": relevance, 
+		"F1rr": F1rr
+	}
+
+	return scores
+
+
+def precision_recall(model, known): 
+
+	## WARNING !! 
+	## This function needs to be verified for accuracy. 
+
+	## TODO: Could potentially be optimized by removing all nan values first.
+
+	# If signal value is non-zero, assign to 1
+	supp1 = (model != 0).astype(float)
+	supp2 = (known != 0).astype(float) 
+
+	# For recall and precision, we need to find number of times pairwise elements appear in known and model modules
+	# Given a boolean module vector, element (i,j) is 1 if and only ifboth i and j are in the module 
+	# To count the number of modules that each pair is in, we need to perform an outer product for each module
+	# This can be efficiently computed with a matrix multiplication
+	pairwise1 = np.matmul(supp1.T, supp1)
+	pairwise2 = np.matmul(supp2.T, supp2)
+	min_pairwise = np.minimum(pairwise1,pairwise2)
+
+	pairwise1[pairwise1 == 0] = np.nan
+	pairwise2[pairwise2 == 0] = np.nan
+	pairwise_norm1 = np.divide(min_pairwise, pairwise1)
+	pairwise_norm2 = np.divide(min_pairwise, pairwise2)
+
+	# We need to take the mean of the lower triangular matrix excluding the diagonal, so we set the upper 
+	# triangular matrix to nan, then take the mean. 
+	pairwise_norm1[np.triu_indices(pairwise_norm1.shape[0])] = np.nan
+	pairwise_norm2[np.triu_indices(pairwise_norm2.shape[0])] = np.nan
+
+	# We can run into problems if the overlap here is sparse
+	if np.count_nonzero(~np.isnan(pairwise_norm1)) == 0: precision = 0
+	else: precision = np.nanmean(pairwise_norm1)
+
+	if np.count_nonzero(~np.isnan(pairwise_norm2)) == 0: recall = 0
+	else: recall = np.nanmean(pairwise_norm2)
+
+	if recall * precision == 0: F1rp = 0
+	else: F1rp = stats.hmean([recall, precision])
+
+	scores = {
+		"precision": precision, 
+		"recall": recall, 
+		"F1rp": F1rp
+	}
+
+	return scores
