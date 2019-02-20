9baa3515a6ad3ee33eff8cae6a9a255555493639
Johnny Li <iamjli@mit.edu>
2019-02-19 20:45:01 -0500

    Implement forest in GSD, move tree version to sepearate file
     * GSD now allows forest components connected by a dummy node with specified weight
     * Parameters are now passed via a dictionary
     * Names of everything related to component and score matrices are now D and Z, respectively.

--- scripts/gsd.py
+++ scripts/gsd.py
@@ -27,71 +27,114 @@ logger.addHandler(handler)
 
 n_cpus = multiprocessing.cpu_count()
 
+
+class Options(object):
+	def __init__(self, options):
+		self.__dict__.update(options)
+	def __repr__(self):
+		return dict(self.__dict__)
+
 class GSD:
 
-	def __init__(self, X, graph, a, n_components, initializer='random'): 
+	def __init__(self, data, edgelist, params=dict()): 
 
-		# Import as networkx object if path is provided
-		if isinstance(graph, str): graph = nx.read_gpickle(graph)
+		defaults = { 
+			"a": 0.4, 
+			"n_components": 3, 
+			"method": "gsPCA", 
+			"initializer": "zeros", 
+			"same_sign": True, 
+			"dummy_nodes": [], 
+			"w": 1
+		}
 
-		self.X = X          # Data matrix (n_samples, n_features) whose features are ordered according to graph.nodes
-		self.graph = graph  # networkx interactome
-		self.a = a          # Sparsity edge constraint 
-		self.n_components = n_components
-		self.n_samples, self.n_features = self.X.shape
+		self.params = Options({**defaults, **params})
+		self.data = data
+		self.edgelist = edgelist
 
-		if self.n_samples > self.n_features: 
-			logger.warn("Number of samples exceeds number of features, class may not function properly!")
-
-		# Get graph edgelist as node indices for PCST.
-		self._set_namespace_mappings()
-		self._edges_pcst = self._to_indices(self.graph.edges)
-		# Get scaled edge costs.
-		self.edge_cost = self._edge_costs()
-
-		# Initialize graph components.
-		if initializer == 'ica': 
-			self.components, self.scores = self._ica_initializer()
-		elif initializer == 'random': 
-			self.components, self.scores = self._random_initializer()
-		elif initializer == 'zeros': 
-			self.components, self.scores = self._zero_initializer()
-		else: 
-			logger.warn("Initializer must be set to `random` or `ica`.")
+		self._initialize_indices()
+		self._initialize_matrices()
 
 		# Initialize edges. Finds the minimum spanning tree to use as a baseline.
-		self.tree_edges = [ self._pcst(np.ones(self.n_features), self.edge_cost*0)[1] ] * self.n_components
+		self.D_edges = [ self._pcsf( prizes=np.ones(self.n_features), costs=self.costs*0 )[1] ] * self.n_components
 		# Initialize objective scores
-		self.objective, self.error, self.tree_cost = self.compute_objective(self.components, self.scores, self.tree_edges)
+		self.objective, self.error, self.D_cost = self.compute_objective(self.D, self.Z, self.D_edges)
 
 
-	########  HELPER FUNCTIONS  ########
+	########  INITIALIZATION  ########
 
-	def _set_namespace_mappings(self): 
-		"""Sets namespace mappings between gene names and node indices."""
-		self._gene_to_index = { gene: i for i,gene in enumerate(self.graph) }
-		self._index_to_gene = { i: gene for i,gene in enumerate(self.graph) }
-	
-	def _to_indices(self, genes): 
-		"""Converts object with gene names to node indices."""
-		return np.vectorize(self._gene_to_index.get)(genes)
-		
-	def _to_genes(self, indices): 
-		"""Converts object with node indices to gene names."""
-		return np.vectorize(self._index_to_gene.get)(indices)
+	def _initialize_indices(self): 
 
-	def _pcst(self, prizes, costs): 
-		"""Wrapper for `pcsf_fast`. Returns vertex and edge indices."""
-		return pcst_fast(self._edges_pcst, prizes, costs, -1, 1, 'strong', 0)
-		
-	def _edge_costs(self): 
-		"""Returns edge costs scaled by `a`."""
-		return self.a * np.array([ cost for _,_,cost in self.graph.edges.data('cost') ])
+		# Convert the interactome dataframe from string interactor IDs to integer interactor IDs.
+		# Do so by selecting the protein1 and protein2 columns from the interactome dataframe,
+		# then unstacking them, which (unintuitively) stacks them into one column, allowing us to use factorize.
+		# Factorize builds two datastructures, a unique pd.Index which maps each ID string to an integer ID,
+		# and the datastructure we passed in with string IDs replaced with those integer IDs.
+		# We place those in self.nodes and self.edges respectively, but self.edges will need reshaping.
+		(self.edges, self.nodes) = pd.factorize(self.edgelist[["anchor1","anchor2"]].unstack())
+		# Here we do the inverse operation of "unstack" above, which gives us an interpretable edges datastructure
+		self.edges = self.edges.reshape(self.edgelist[["anchor1","anchor2"]].shape, order='F')
+
+		self.costs = self.edgelist["cost"].astype(float).values
+
+		# Add dummy nodes. If `self.params.dummy_nodes` is empty list, these edges will be unchanged. 
+		endpoints = np.where(self.nodes.isin(self.params.dummy_nodes))[0]
+		self._dummy_id = len(self.nodes)
+		if len(endpoints) > 0: 
+			# Add edges between dummy node and each endpoint
+			self.edges = np.concatenate((self.edges, np.array([(self._dummy_id, node_id) for node_id in endpoints])))
+			# Assign costs of dummy edges to be w
+			self.costs = np.concatenate((self.costs, np.array([self.params.w] * len(endpoints))))
+
+	def _initialize_matrices(self): 
+
+		# Reindex new dataframe to match node indices
+		self.data = self.data.reindex(columns=self.nodes).fillna(0)
+		self.X = self.data.values
+
+		(self.n_samples, self.n_features) = self.data.shape
+		self.n_components = self.params.n_components
+
+		# Initialize submatrices
+		if self.params.initializer == "random": 
+			self.D = np.random.normal(size=(self.n_components, self.n_features)) 
+			self.Z = self._update_Z(self.D)
+
+		elif self.params.initializer == "zeros": 
+			self.D = np.zeros(shape=(self.n_components, self.n_features)) 
+			self.Z = self._update_Z(self.D) 
+
+		elif self.params.initializer == "ica": 
+			ica = FastICA(n_components=self.n_components)
+			self.D = ica.fit_transform(self.X.T).T  
+			self.Z = ica.mixing_ 
+
+		else: 
+			pass
 
 
 	########  DICTIONARY LEARNING  ########
 
-	def update(self, i, method='gsPCA', same_sign=True): 
+	def _pcsf(self, prizes, costs):
+
+		if len(self.params.dummy_nodes) == 0: 
+			# Unrooted PCST
+			node_indices, edge_indices = pcst_fast(self.edges, prizes, costs, -1, 1, "strong", 0)
+
+		else: 
+			# This is an alternative implementation of PCSF that roots the dummy node by assigning
+			# a prize to it such that the prize is slightly larger than the cost of its associated
+			# edges. This method of rooting runs faster than rooting the graph explicitly using the 
+			# `root` parameter in the `pcst_fast` function. 
+			prizes = np.concatenate((prizes, [ max(costs) * 1.01 ]))
+			node_indices, edge_indices = pcst_fast(self.edges, prizes, costs, -1, 1, "strong", 0)
+			# Remove references to dummy node
+			node_indices = node_indices[node_indices != self._dummy_id]
+			edge_indices = self.edgelist.index & edge_indices
+
+		return node_indices, edge_indices
+
+	def update(self, i): 
 		"""
 		Updates component and scores matrix together along with associated tree_edgelist and model metrics
 
@@ -99,22 +142,18 @@ class GSD:
 			i (int): component index
 			same_sign (bool): if True, component values must be all positive or all negative
 		"""
-		new_D, new_tree_i = self._update_component(i, self.components, self.scores, method=method, same_sign=same_sign)
-		new_Z = self._update_scores(new_D)
+		new_D, new_D_edges = self._update_D(i, self.D, self.Z)
+		new_Z = self._update_Z(new_D)
 
 		# Check if updated matrices improve upon objective.
 		if True: 
-			self.components = new_D
-			self.scores = new_Z
-			self.tree_edges[i] = new_tree_i
+			self.D = new_D
+			self.Z = new_Z
+			self.D_edges[i] = new_D_edges
 
-			self.objective, self.error, self.tree_cost = self.compute_objective(self.components, self.scores, self.tree_edges)
+			self.objective, self.error, self.D_cost = self.compute_objective(self.D, self.Z, self.D_edges)
 
-		# Check if new component is too small or only one element, then remove. 
-		# TODO
-
-
-	def _update_component(self, i, D, Z, method='gsPCA', same_sign=True): 
+	def _update_D(self, i, D, Z): 
 		"""
 		Updates component i of dictionary D while fixing the rest of the components and scores Z to be constant.
 
@@ -137,12 +176,12 @@ class GSD:
 		# Get residual with component i held out
 		X_res = self.X - np.dot(np.delete(Z, i, axis=1), np.delete(D, i, axis=0))
 
-		if method == 'dict_learning': 
+		if self.params.method == 'dict_learning': 
 			# Calculate optimal D_i via least squares estimation (in other words, what's the 
 			# best component assuming no sparsity constraint)
 			Z_i = Z[:,i]
 			optimal_D_i = np.dot(Z_i, Z_i) ** (-1) * np.dot(Z_i, X_res)
-		elif method == 'gsPCA': 
+		elif self.params.method == 'gsPCA': 
 			# Model component after graph-sparse PC1
 			pca = PCA(n_components=1)
 			Z_i = pca.fit_transform(X_res).T[0]
@@ -157,7 +196,7 @@ class GSD:
 		# This is calculated by taking the difference in mean reconstruction error with and without the node.
 		prizes = (X_res ** 2).mean(axis=0) - ((X_res - np.outer(Z_i, optimal_D_i)) ** 2).mean(axis=0)
 
-		if same_sign: 
+		if self.params.same_sign: 
 			# Require values within each component to have the same sign by setting values of the opposite sign to 0.
 			prizes[ optimal_D_i < 0 ] = 0
 
@@ -165,7 +204,7 @@ class GSD:
 		# prizes[ abs(prizes) < 1e-8 ] = 0
 		prizes = prizes.clip(min=0) 
 
-		node_indices, edge_indices = self._pcst(prizes, self.edge_cost)
+		node_indices, edge_indices = self._pcsf(prizes, self.costs * self.params.a)
 
 		if len(node_indices) <= 1: 
 			logger.warn("Component {} is deprecated!".format(i))
@@ -177,17 +216,13 @@ class GSD:
 		return D, edge_indices
 
 
-	def _update_scores(self, D): 
+	def _update_Z(self, D): 
 		"""Updates scores matrix by fixing dictionary D."""
-
+		Z = np.zeros( shape=(self.n_samples, self.n_components) )
 		# Component row indices that are not empty
 		nonempty_indices = np.where(D.any(axis=1))[0]
-
-		# For components that are nonempty, set values using OLS. Otherwise, set values to 0. 
-		scores = np.zeros( shape=(self.n_samples, self.n_components) )
-		scores[:,nonempty_indices] = self._OLS(self.X, D[nonempty_indices])
-
-		return scores
+		Z[:,nonempty_indices] = self._OLS(self.X, D[nonempty_indices])
+		return Z
 
 	def _OLS(self, X, D):
 		"""
@@ -201,8 +236,11 @@ class GSD:
 			numpy.array: 2D scores array indicating proportions of each component (n_samples, n_components)
 		"""
 		return np.linalg.inv(D.dot(D.T)).dot(D).dot(X.T).T
-	
-	def compute_objective(self, D, Z, tree_edges): 
+
+
+	########  CONVENIENCE  ########
+
+	def compute_objective(self, D, Z, D_edges): 
 		"""
 		Computes objective score for components D and scores Z
 
@@ -218,97 +256,36 @@ class GSD:
 		"""
 		
 		# Total cost of edges for all trees
-		tree_cost = sum([ sum(self.edge_cost[edges]) for edges in tree_edges ])
+		D_cost = sum([ sum(self.costs[edges]) for edges in D_edges ])
 		# Reconstruction error
 		error = ((self.X - Z.dot(D)) ** 2).sum() / self.n_samples
 		# Objective score
-		objective = error + tree_cost
+		objective = error + D_cost
 		
-		return objective, error, tree_cost
-
-
-	########  INITIALIZATION  ########
-	
-	def _random_initializer(self): 
-		"""Initializes dictionary and scores matrices randomly."""
-		components = np.random.normal( size=(self.n_components, self.n_features) ) 
-		scores     = self._OLS( self.X, components ) # (n_samples, n_components)
-		logger.info("Initialized components and scores randomly.")
-		
-		return components, scores
-
-
-	def _zero_initializer(self): 
-		"""Initializes dictionary and scores matrices randomly."""
-		components = np.zeros( shape=(self.n_components, self.n_features) ) 
-		scores     = self._update_scores( components ) # (n_samples, n_components)
-		logger.info("Initialized components and scores with zeros.")
-		
-		return components, scores
-
-
-	def _ica_initializer(self): 
-		"""Initializes dictionary and scores matrices with independent component analysis (ICA)."""
-		ica = FastICA(n_components=self.n_components)
-		# Because n_samples < n_features, we must transpose
-		sources = ica.fit_transform(self.X.T).T  # (n_sources, n_features)
-		mixing  = ica.mixing_  # (n_samples, n_sources)
-		logger.info("Initialized components and scores via ICA.")
-
-		return sources, mixing
-
-
-	########  CONVENIENCE  ########
-
-
-	def supports(self): 
-		"""Gets component supports."""
-		return (self.components != 0).astype(int)
+		return objective, error, D_edges
 
 
 	def steiner_supports(self): 
 		"""Gets components that may contain Steiner nodes."""
-		indices = [ np.unique(self._edges_pcst[edges]) for edges in self.tree_edges ]
-		out = np.array([[ 1 if x in arr else 0 for x in range(self.n_features) ] for arr in indices])
-		return out
+		supports = np.zeros(shape=(self.n_components, self.n_features), dtype=int)
+		for i,edges in enumerate(self.D_edges): 
+			supports[i,np.unique(self.edges[edges])] = 1
+		return supports
 
 
-	def component_genes(self, i): 
+	def output_normalized_decomposition(self): 
 
-		indices = np.unique(self._edges_pcst[self.tree_edges[i]])
-		return self._to_genes(indices)
+		D_scale = (self.D ** 2).sum(axis=1) ** 0.5
+		D_norm = self.D * np.expand_dims(1/D_scale, axis=1)
+		component_norm = pd.DataFrame(D_norm, columns=self.data.columns)
 
+		Z_scale = (self.Z ** 2).sum(axis=0) ** 0.5
+		Z_norm = self.Z * np.expand_dims(1/Z_scale, axis=0)
+		score_norm = pd.DataFrame(Z_norm, index=self.data.index)
 
-	def component_graph(self, i): 
+		singular_values = D_scale * Z_scale
 
-		component_edges = np.array(self.graph.edges)[self.tree_edges[i]]
-		component_graph = self.graph.edge_subgraph([tuple(edge) for edge in component_edges]).copy()
+		# X ~ (normalized_scores * singular_values).dot(normalized_components)
 
-		if "DUMMY" in component_graph.nodes: 
-			component_graph.remove_node("DUMMY")
-
-		return component_graph
-
-
-	def log_objectives(self): 
-
-		# logger.info("{0:.2f}\t{0:.2f}\t{0:.2f}".format(self.objective, self.error, self.tree_cost))
-		logger.info("{}\t{}\t{}".format(int(self.objective), int(self.error), int(self.tree_cost)))
-
-
-	def output_normalized_decomposition(self, sample_labels, gene_labels): 
-
-	    scale_components = (self.components ** 2).sum(axis=1) ** 0.5
-	    normalized_components = self.components * np.expand_dims(1/scale_components, axis=1)
-	    normalized_components = pd.DataFrame(normalized_components, columns=gene_labels)
-
-	    scale_scores = (self.scores ** 2).sum(axis=0) ** 0.5
-	    normalized_scores = self.scores * np.expand_dims(1/scale_scores, axis=0)
-	    normalized_scores = pd.DataFrame(normalized_scores, index=sample_labels)
-
-	    singular_values = scale_components * scale_scores
-
-	    # X ~ (normalized_scores * singular_values).dot(normalized_components)
-
-	    return normalized_scores, singular_values, normalized_components
+		return score_norm, singular_values, component_norm
 
